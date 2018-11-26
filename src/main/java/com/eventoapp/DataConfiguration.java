a41a85e1e7a44bd9b35a9a841748de07247758c6
lmmendonca <lmaximino.mendonca@gmail.com>
2018-11-26 19:56:14 -0200

    configurando PostgresSql

--- src/main/java/com/eventoapp/DataConfiguration.java
+++ src/main/java/com/eventoapp/DataConfiguration.java
@@ -3,9 +3,14 @@ package com.eventoapp;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import javax.sql.DataSource;
 
 @Configuration
 public class DataConfiguration {
@@ -24,5 +29,25 @@ public class DataConfiguration {
         basicDataSource.setPassword(password);
 
         return basicDataSource;
+    }
+
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("@#1B1oaf");
+//        return dataSource;
+//    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.POSTGRESQL);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("rg.hibernate.dialect.PostgreSQLDialect");
+        adapter.setPrepareConnection(true);
+        return adapter;
     }
 }
